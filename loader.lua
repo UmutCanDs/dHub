2a780576541b8ce3512bd745cb0eacdf11128519
UmutCanDs <87336701+UmutCanDs@users.noreply.github.com>
2022-10-10 19:43:47 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 730951264 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UmutCanDs/dHub/main/730951264"))()
 elseif game.PlaceId == 6608498361 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UmutCanDs/dHub/main/6608498361"))()
+elseif game.PlaceId == 6654918151 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/UmutCanDs/dHub/main/6654918151"))()
 end
